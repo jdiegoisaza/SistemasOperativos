6d989b488447f33f71ba58ecbec0cd88e95690c8
Juan Isaza <39199649+jdiegoisaza@users.noreply.github.com>
2018-08-27 12:05:45 -0500

    Update sol_ex4.c

--- sol_ex4.c
+++ sol_ex4.c
@@ -1,32 +1,5 @@
-#include <stdio.h>
-
-
-/*********************************************************/
-/*            Declaraciones de las funciones             */
-/*********************************************************/
-
-/* Funciones de test */
-void testVolverMayuscula(void);
-void testEsLetra(void);
-void testStringToMayuscula(void);
-
-/* Funciones del programa */
-int esLetra(char ch);
-void volverMayuscula(char *ch);
-void stringToMayuscula(char s[]);
-
-
-/*********************************************************/
-/*                     Funcion main                      */
-/*********************************************************/
-
-
-int main(void) {
-    
-    char *palabra;
     printf("Ingrese palabra: ");
     scanf("%s",palabra);
-
     stringToMayuscula(palabra);
 
   /*testVolverMayuscula();
@@ -34,17 +7,6 @@ int main(void) {
   testStringToMayuscula();
   */return 0;
 }
-
-/*********************************************************/
-/*            Declaraciones de las funciones             */
-/*********************************************************/
-
-#include <stdio.h>
-
-/* Funciones del programa */
-int esLetra(char ch);
-void volverMayuscula(char *ch);
-void stringToMayuscula(char s[]);
 
 /*********************************************************/
 /*             Definiciones de las funciones             */
@@ -59,10 +21,9 @@ void stringToMayuscula(char s[]);
  *   @return 1 si el caracter es una letra del alfabeto y 0 si es otro simbolo.
  */
 int esLetra(char ch) {
-  // Coloque el codigo solucion a continuacion...
-
+  if(ch>96&&ch<123)return 1;
+  else return 0;
 }
-
 /**  
  *   @brief  Convierte un caracter en mayuscula
  *  
@@ -70,19 +31,26 @@ int esLetra(char ch) {
  *   @return void
  */
 void volverMayuscula(char *ch) {
-  // Coloque el codigo solucion a continuacion...
+  *ch=*ch-32;
 
 }
-
-
 /**  
  *   @brief  Convierte en mayusculas la cadena de caracteres ingresada.
  *  
- *   @param  s es una cadena de caracteres ingresada y la cual despues del proceso en la función tendra los caracteres en mayuscula.
+ *   @param  s es una cadena de caracteres ingresada y la cual despues del proceso en la funciC3n tendra los caracteres en mayuscula.
  *   @return void
  */
-
 void stringToMayuscula(char s[]) {
-  
+  char *p1,n;
+  //p1=s;
+  while(strcmp(s, "\0") != 0){
+    p1=s;
+    int i=esLetra(*p1);
+    if(i==1){
+      volverMayuscula(p1);
+    }
+  printf("%c",*p1);
+    s=s+1;
+  }
 
 }
