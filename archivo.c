08a667ba28ae854dad95922fcc9671fad09565c4
Juan Isaza <39199649+jdiegoisaza@users.noreply.github.com>
2018-09-14 13:36:26 -0500

    Update archivo.c

--- archivo.c
+++ archivo.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 
+
 int main() {
   srand(time(NULL)); // Inicializacion del generador
   char filename[80];
   char line[201];
-  int cantChar;
-  char ch;
+  int cantChar,cantLin,cantPal;
+  char ch,pal;
   char *p1;
-  FILE *inputFile;
+  FILE *inputFile,*copiaC,*copiaL;
   printf("Ingrese el nombre de la cadena de ADN a generar: ");
   scanf("%[^\n]s",filename); // Formato para que la entrada pueda aceptar espacios
   inputFile = fopen(filename,"r");
@@ -17,20 +18,34 @@ int main() {
     printf("Error al abrir el archivo %s\n", filename);
     exit(-1);
   }
+  copiaC=fopen(filename,"r");
+  copiaL=fopen(filename,"r");
   /*//imprime la cantidad de caracteres
   do {
-    ch = getc(inputFile);    
+    ch = getc(copiaC); 
+    cantChar++; 
   } while(ch != EOF);
-  printf("LA cantidad de caracteres es: %d\n",cantChar);*/
+  printf("LA cantidad de caracteres es: %d\n",cantChar);
 
     //imprime la cantidad de lineas
-  while(fgets(line, 201, inputFile)!=NULL) {
-    printf("%-5d",cantChar++);
+  while(fgets(line, 1000, inputFile)!=NULL) {
+    printf("%-5d",++cantLin);
     printf("%s",line);
   }
-  printf("LA cantidad de lineas es: %d\n",cantChar);
-
-
+  printf("LA cantidad de lineas es: %d\n",cantLin);
+    */
+  //Cuenta las palabras
+  int bol=1;
+  do {
+    pal = getc(copiaC); 
+    if(pal==' '||pal=='\n'){
+        if(bol=0){cantPal++;} 
+        bol=1;
+    }else{
+        bol=0;
+    }
+  } while(pal != EOF);
+  printf("La cantidad de palabras es: %d\n",cantPal);
 
   printf("\n");
   fclose(inputFile);
