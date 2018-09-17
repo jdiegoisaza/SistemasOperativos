ec2c9c380cfc238899bba20d81fbb49691d0b7a5
Juan Isaza <39199649+jdiegoisaza@users.noreply.github.com>
2018-09-17 15:47:15 -0500

    Update archivo.c

--- archivo.c
+++ archivo.c
@@ -7,8 +7,8 @@ int main() {
   srand(time(NULL)); // Inicializacion del generador
   char filename[80];
   char line[201];
-  int cantChar,cantLin,cantPal;
-  char ch,pal;
+  int cantChar,cantLin,cantPal,cantWh,cantMy,cantMn,cantDi;
+  char ch,pal,wh,my,mn,di;
   char *p1;
   FILE *inputFile,*copiaC,*copiaL;
   printf("Ingrese el nombre de la cadena de ADN a generar: ");
@@ -26,26 +26,54 @@ int main() {
     cantChar++; 
   } while(ch != EOF);
   printf("LA cantidad de caracteres es: %d\n",cantChar);
-
     //imprime la cantidad de lineas
   while(fgets(line, 1000, inputFile)!=NULL) {
-    printf("%-5d",++cantLin);
+    printf("%-5d",++cantLin);//muestra el numero de la linea en la que se encuentra
     printf("%s",line);
   }
   printf("LA cantidad de lineas es: %d\n",cantLin);
-    */
+
   //Cuenta las palabras
   int bol=1;
   do {
-    pal = getc(copiaC); 
-    if(pal==' '||pal=='\n'){
-        if(bol=0){cantPal++;} 
+    pal = getc(inputFile); 
+    if(pal==' '||pal=='\n'||pal==','||pal=='.'){//aclaramos que los caracteres , y . no cuentan como palabra
+        if(bol==0){cantPal++;}
         bol=1;
     }else{
         bol=0;
     }
   } while(pal != EOF);
   printf("La cantidad de palabras es: %d\n",cantPal);
+    do {
+    wh = getc(inputFile); 
+    if(wh==' '||wh=='\n'){
+        cantWh++;
+    }
+  } while(wh != EOF);
+  printf("La cantidad de whitespaces o saltos es: %d\n",cantWh);
+  do {
+    my = getc(inputFile); 
+    if(my<91&&my>64){
+        cantMy++;
+    }
+  } while(my != EOF);
+  printf("La cantidad de Mayusculas es: %d\n",cantMy);
+  do {
+    mn = getc(inputFile); 
+    if(mn<123&&mn>96){
+        cantMn++;
+    }   
+  } while(mn != EOF);
+  printf("La cantidad de minusculas es: %d\n",cantMn);*/
+  do {
+    mn = getc(inputFile); 
+    if(di<58&&di>47){
+        cantDi++;
+    }
+  } while(di != EOF);
+  printf("La cantidad de digitos es: %d\n",cantDi);
+
 
   printf("\n");
   fclose(inputFile);
