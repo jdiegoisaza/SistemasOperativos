80547f2925f058fc3e249a0b36816bf77200974d
Juan Isaza <39199649+jdiegoisaza@users.noreply.github.com>
2018-09-18 00:42:02 -0500

    Update archivo.c

--- archivo.c
+++ archivo.c
@@ -1,42 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 
-int main() {
+int main(int argc,char *argv[]) {
   srand(time(NULL)); // Inicializacion del generador
-  char filename[80];
   char line[201];
-  int cantChar,cantLin,cantPal,cantWh,cantMy,cantMn,cantDi;
-  char ch,pal,wh,my,mn,di;
-  char *p1;
-  FILE *inputFile,*copiaC,*copiaL;
-  printf("Ingrese el nombre de la cadena de ADN a generar: ");
-  scanf("%[^\n]s",filename); // Formato para que la entrada pueda aceptar espacios
-  inputFile = fopen(filename,"r");
-  if (inputFile == NULL) {
-    printf("Error al abrir el archivo %s\n", filename);
+  int cantChar=0,cantLin=0,cantPal=0,cantWh=0,cantMy=0,cantMn=0,cantDi=0;
+  char ch,pal,wh,my,mn,di,*cad,*cad2;
+  FILE *copiaC,*copiaL,*copiaP,*copiaW,*copiaMa,*copiaMi,*copiaD,*output;
+  cad=argv[1];
+  copiaC=fopen(cad,"r");
+  if (copiaC == NULL) {
+    printf("Error al abrir el archivo %s\n", cad);
     exit(-1);
   }
-  copiaC=fopen(filename,"r");
-  copiaL=fopen(filename,"r");
-  /*//imprime la cantidad de caracteres
+  printf("Obteniendo estadisticas...\n");
+  //imprime la cantidad de caracteres
   do {
     ch = getc(copiaC); 
     cantChar++; 
   } while(ch != EOF);
-  printf("LA cantidad de caracteres es: %d\n",cantChar);
+  printf("chars: %d\n",cantChar);
+  fclose(copiaC);
     //imprime la cantidad de lineas
-  while(fgets(line, 1000, inputFile)!=NULL) {
-    printf("%-5d",++cantLin);//muestra el numero de la linea en la que se encuentra
-    printf("%s",line);
+  copiaL=fopen(cad,"r");
+  while(fgets(line, 1000, copiaL)!=NULL) {
+    ++cantLin;
   }
-  printf("LA cantidad de lineas es: %d\n",cantLin);
-
+  printf("lines: %d\n",cantLin);
+  fclose(copiaL);
   //Cuenta las palabras
+  copiaP=fopen(cad,"r");
   int bol=1;
   do {
-    pal = getc(inputFile); 
+    pal = getc(copiaP); 
     if(pal==' '||pal=='\n'||pal==','||pal=='.'){//aclaramos que los caracteres , y . no cuentan como palabra
         if(bol==0){cantPal++;}
         bol=1;
@@ -44,38 +43,59 @@ int main() {
         bol=0;
     }
   } while(pal != EOF);
-  printf("La cantidad de palabras es: %d\n",cantPal);
-    do {
-    wh = getc(inputFile); 
+  printf("words: %d\n",cantPal);
+  fclose(copiaP);
+  //Cuenta whitespaces
+  copiaW=fopen(cad,"r");
+  do {
+    wh = getc(copiaW); 
     if(wh==' '||wh=='\n'){
         cantWh++;
     }
   } while(wh != EOF);
-  printf("La cantidad de whitespaces o saltos es: %d\n",cantWh);
+  printf("whitespaces: %d\n",cantWh);
+  fclose(copiaW);
+  //Cuenta Mayusculas
+  copiaMa=fopen(cad,"r");
   do {
-    my = getc(inputFile); 
+    my = getc(copiaMa); 
     if(my<91&&my>64){
         cantMy++;
     }
   } while(my != EOF);
-  printf("La cantidad de Mayusculas es: %d\n",cantMy);
+  printf("uppercases: %d\n",cantMy);
+  fclose(copiaMa);
+  //Cuenta minusculas
+  copiaMi=fopen(cad,"r");
   do {
-    mn = getc(inputFile); 
+    mn = getc(copiaMi); 
     if(mn<123&&mn>96){
         cantMn++;
     }   
   } while(mn != EOF);
-  printf("La cantidad de minusculas es: %d\n",cantMn);*/
+  printf("lowercases: %d\n",cantMn);
+  fclose(copiaMi);
+  //Cuenta digitos
+  copiaD=fopen(cad,"r");
   do {
-    mn = getc(inputFile); 
+    di = getc(copiaD); 
     if(di<58&&di>47){
         cantDi++;
     }
   } while(di != EOF);
-  printf("La cantidad de digitos es: %d\n",cantDi);
-
-
+  printf("digits: %d\n",cantDi);
+  fclose(copiaD);
+  cad2=strcat( cad, "_stats.txt" );
+  printf("%s --> generando reporte %s\n",cad,cad2);
+  printf("Estadisticas culminadas\n");
+  output=fopen(cad2,"w");
+  fputs("chars: ", output);
+  fputs("\nlines: ", output);
+  fputs("\nwords: ", output);
+  fputs("\nwhitespaces: ", output);
+  fputs("\nuppercasess: ", output);
+  fputs("\nlowercases: ", output);
+  fputs("\ndigits: ", output);
   printf("\n");
-  fclose(inputFile);
   exit(0);
 }
